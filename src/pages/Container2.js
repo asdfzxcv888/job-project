7befc05d06a090df719e4c87d9f4cc069ee09afb
unknown <mendegowtham4131@gmail.com>
2024-03-21 00:07:19 +0530

    first2

--- src/pages/Container2.js
+++ src/pages/Container2.js
@@ -3,8 +3,8 @@ import { IoEyeOutline } from "react-icons/io5";
 
 const Container2 = () => {
     return (
-      <div className='contain2'>
-        <p> <IoEyeOutline></IoEyeOutline> Filled Name</p>
+      <div className='contain2' >
+        <div className='specialp' style={{display:'inline-block',width:'10%'}}> <IoEyeOutline></IoEyeOutline> Filled Name</div>
           <p>Jan 12,2024</p>
           <p>jan 14,2024</p>
           <p>Muhammad Azad</p>
